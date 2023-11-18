0c1f6580afdaa07006e9f71b37669ea639f71479
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-11-18 23:24:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getFormvalue() {
     //Write your code here
+  const form = document.getElementById('form1');
+  const firstName = form.elements['fname'].value.trim();
+  const lastName = form.elements['lname'].value.trim();
+
+  if (firstName === '' || lastName === '') {
+    alert('Please enter both first and last names.');
+  } else {
+    alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
+  }
 
 }
+document.getElementById('userForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevents the default form submission
+  getFormvalue();
+});
