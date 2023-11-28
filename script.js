62ed9c7c6a661db95c2396b9550ce35e0e085e54
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-11-28 23:25:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function getFormvalue() {
     //Write your code here
-  const form = document.getElementById('form1');
-  function getFormvalue() {
-     var x=document.getElementById("form1");
-     let name = ""
-     for (var i=0;i<x.length;i++) {
-     if (x.elements[i].value!='Submit')
-         name += x.elements[i].value + " "
-     }
+   var x=document.getElementById("form1");
+   let name = ""
+   for (var i=0;i<x.length;i++) {
+   if (x.elements[i].value!='Submit')
+      name += x.elements[i].value + " "
+   }
 
-     alert(name.substring(0,name.length-1));
-  }
-
+alert(name.substring(0,name.length-1));
 }
 document.getElementById('userForm').addEventListener('submit', function(event) {
   event.preventDefault(); // Prevents the default form submission
